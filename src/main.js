68cdf1ffd52931a369ab7456a93cdd542f704b30
Nicoly Cunha <nicolyoliveiracunha.2001@gmail.com>
2025-01-21 21:22:15 -0300

    feat: load client card dynamically

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+const base_url = 'http://localhost:3333'
 const form = document.querySelector('form')
 const inputId = document.getElementById('id')
-const base_url = 'http://localhost:3333'
+
+const clientCard = document.querySelector('.card-client')
 
 form.onsubmit = async event => {
     event.preventDefault()
@@ -9,11 +11,60 @@ form.onsubmit = async event => {
         const id = inputId.value.trim()
 
         const result = await fetch(`${base_url}/clients/${id}`)
+
+        if (result.status == 404) {
+            alert('Identificador de cartão fidelidade inválido!')
+            return
+        }
+
         const data = await result.json()
 
-        console.log(data)
+        clearCards()
+        createClientCard(data)
     } catch (error) {
         console.log(error)
-        alert('Identificador de cartão fidelidade inválido!')
+        alert('Não foi possível carregar os dados deste cartão fidelidade!')
+    }
+}
+
+function clearCards() {
+    clientCard.innerHTML = ''
+}
+
+function createClientCard(data) {
+    const photo = getClientCardPhoto(data)
+    const info = getClientCardInfo(data)
+
+    clientCard.append(photo, info)
+}
+
+function getClientCardPhoto(data) {
+    try {
+        const photo = document.createElement('img')
+        photo.setAttribute('src', `src/assets/images/profile/${data.photo}`)
+        photo.setAttribute('alt', `Foto de ${data.name}`)
+
+        return photo
+    } catch (error) {
+        throw error
+    }
+}
+
+function getClientCardInfo(data) {
+    try {
+        const info = document.createElement('div')
+        info.classList.add('info')
+
+        const name = document.createElement('h2')
+        name.textContent = data.name
+
+        const clientSince = document.createElement('p')
+        clientSince.textContent = `Cliente desde ${data.clientSince}`
+
+        info.append(name, clientSince)
+
+        return info
+    } catch (error) {
+        throw error
     }
 }
